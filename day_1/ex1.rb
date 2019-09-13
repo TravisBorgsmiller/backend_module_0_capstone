a9ce5dfbc7836a6a754dcedef1d66d5dc1aa6abf
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-13 03:13:48 -0600

    add study drill

--- day_1/ex1.rb
+++ day_1/ex1.rb
@@ -5,3 +5,16 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+#Print another line
+puts "Another line"
+#print just one line
+puts "Hello World!"
+=begin Commented out rest of code
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+=end
