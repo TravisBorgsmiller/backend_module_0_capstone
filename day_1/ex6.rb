74f93f17f2d81acf000250767c202aa04aa59291
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-14 04:59:06 -0600

    add study drills

--- day_1/ex6.rb
+++ day_1/ex6.rb
@@ -1,21 +1,35 @@
+#variables for types of people
 types_of_people = 10
+#insert types_of_people variables into string
 x = "There are #{types_of_people} types of people."
+#declare variable
 binary = "binary"
+#declare variable
 do_not = "don't"
+#insert variables into string
 y = "Those who know #{binary} and those who #{do_not}."
-
+#print variable string
 puts x
+#print variable string
 puts y
-
-puts "I said: #{x}."
-puts "I also said: '#{y}'."
-
+#print string with variable x inserted
+puts "I said: #{x}"
+#print string with variable y inserted
+puts "I also said: #{y}"
+#declare boolean
 hilarious = false
+#insert variable into string
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
-
+#print variable which contains string
 puts joke_evaluation
-
+#set variable equal to a string
 w = "This is the left side of..."
+#set variable equal to a string
 e = "a string with a right side."
-
+#print two variables that hold strings
 puts w + e
+
+#Study Drills
+#4 places = binary, do_not, x, y all inserted into a string
+#the variables w and e both contain a string, and by adding the contents together, you get an even longer string
+#using  single quotes interferes with the declaration of the do_not variable, because the string thinks that it is ending at the apostrophe in don't
