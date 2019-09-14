f6752c0077ab75540bd89a7e49fcebd8eaf47899
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-14 02:42:18 -0600

    add comments

--- day_1/calculations.rb
+++ day_1/calculations.rb
@@ -1,10 +1,15 @@
 #title
 puts "Weekly breakdown of my time by hour"
+#calculation to get hours in a day
 hours_in_day = 24
 days_in_week = 7
 hours_in_week = hours_in_day * days_in_week
 puts "There are #{hours_in_week} hours in a week"
+#hours spent coding
 puts "I spend 80 hours a week coding"
+#hours left over after coding
 puts "This leaves me with #{164 - 80} hours left in a week"
+#amount of sleep calculation
 puts "After factoring in 8 hours of sleep each day, I am left with #{84 - 56} hours a week"
+#total left over hours a day
 puts "This is #{28 / 7} hours each day"
