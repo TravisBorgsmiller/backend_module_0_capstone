075480b731b607eae45a9ce4eb3c6a44d47fdff8
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-14 05:07:23 -0600

    add study drills

--- day_1/ex11.rb
+++ day_1/ex11.rb
@@ -6,3 +6,12 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+#gets.chomp asks for user feedback and removes the line break
+#can get user feedback
+#add some more question lines
+print "What state do you live in?"
+state = gets.chomp
+print "Where did you go to college?"
+college = gets.chomp
