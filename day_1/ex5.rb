ce6be0b178e52c81ab77c6ad590a085c33fbe399
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-14 04:40:55 -0600

    add study drills

--- day_1/ex5.rb
+++ day_1/ex5.rb
@@ -15,3 +15,26 @@ puts "His teeth are usually #{my_teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+#Study drill without the my_
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+#conversion variables
+one_inch = 2.54 # centimeters
+one_lb = 0.45 # kilograms
