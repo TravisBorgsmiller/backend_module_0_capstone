2a7b09836c4c348653da0eeac7a27bf9f5411b20
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-14 02:53:39 -0600

    add study drill

--- day_1/ex3.rb
+++ day_1/ex3.rb
@@ -10,16 +10,45 @@ puts "Now I will count the eggs:"
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 #prints string
 puts "Is it true that 3 + 2 < 5 - 7?"
-#prints calculation 
+#prints calculation
 puts 3 + 2 < 5 - 7
-
+#adding 3 to 2
 puts "What is 3 + 2? #{3 + 2}"
+#subtracting 7 from 5
 puts "What is 5 - 7? #{5 - 7}"
+#string about previous result
+puts "Oh, that's why it's false."
+#more problems
+puts "How about some more."
+#asks which is greater
+puts "Is it greater? #{5 > -2}"
+#asks if its greater or equal
+puts "Is it greater or equal? #{5 >= -2}"
+#asks if its less than or equal
+puts "Is it less or equal? #{5 <- -2}"
+
+#Rewrite using floating point numbers
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
 
 puts "Oh, that's why it's false."
 
 puts "How about some more."
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <- -2}"
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
