a7125fcb7e0c04553dd8c96ea45de0d196cc783f
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-09-16 22:52:40 -0600

    add exercise

---
+++ day_3/ex29.rb
@@ -0,0 +1,76 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+
+if people == dogs
+  puts "People are dogs."
+end
+
+#It runs the code if the code next to it occus
+#I am pretty sure it is convention
+#I would think it would still runs
+
+if people > cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people == cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people > dogs
+  puts "The world is drooled on!"
+end
+
+if people < dogs
+  puts "The world is dry!"
+end
+
+
+dogs += 5
+
+if people <= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people >= dogs
+  puts "People are less than or equal to dogs."
+end
+
+
+if people >= dogs
+  puts "People are dogs."
+end
+
+
+#You can, and it changes the outcome of which statements are printed
+#This also changes which statements are true or false and what gets printed
